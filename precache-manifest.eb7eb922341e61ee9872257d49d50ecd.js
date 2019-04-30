a818832bae38cb81b631aae350558b52e5e6831b
JP Rickabaugh <jprickabaugh@gmail.com>
2019-04-30 06:04:01 -0400

    Site Files added

---
+++ precache-manifest.eb7eb922341e61ee9872257d49d50ecd.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c8445d66e450d452897ed6fe36f334",
+    "url": "/spotify-project/index.html"
+  },
+  {
+    "revision": "88725545efac79276fd3",
+    "url": "/spotify-project/static/css/2.12eae2ac.chunk.css"
+  },
+  {
+    "revision": "47ca231d6f40d07f7996",
+    "url": "/spotify-project/static/css/main.7f6571a9.chunk.css"
+  },
+  {
+    "revision": "88725545efac79276fd3",
+    "url": "/spotify-project/static/js/2.acdc5f95.chunk.js"
+  },
+  {
+    "revision": "47ca231d6f40d07f7996",
+    "url": "/spotify-project/static/js/main.98821bb0.chunk.js"
+  },
+  {
+    "revision": "2e99809cd4af4dfb7cc4",
+    "url": "/spotify-project/static/js/runtime~main.a9ff3e74.js"
+  },
+  {
+    "revision": "7b8861fb33d4c9ccf25bba64ed94f27f",
+    "url": "/spotify-project/static/media/2Records.7b8861fb.png"
+  },
+  {
+    "revision": "944fd01a967c8d2d5c0f7e8848721c13",
+    "url": "/spotify-project/static/media/4Records.944fd01a.png"
+  },
+  {
+    "revision": "faf6357c2587ba4217fe84fcf32c5604",
+    "url": "/spotify-project/static/media/BrunoMars.faf6357c.png"
+  },
+  {
+    "revision": "b5634d07ed4f6a6cf8ce21c1ebf8716d",
+    "url": "/spotify-project/static/media/CalvinHarris.b5634d07.png"
+  },
+  {
+    "revision": "27a06955058c30f778e61cb15caafef6",
+    "url": "/spotify-project/static/media/Chainsmokers.27a06955.png"
+  },
+  {
+    "revision": "ecfed9f6324d6d83056b014a9cb8c7cc",
+    "url": "/spotify-project/static/media/CleanBandit.ecfed9f6.png"
+  },
+  {
+    "revision": "200729135fe6597f53f8be4310113213",
+    "url": "/spotify-project/static/media/DJKhaled.20072913.png"
+  },
+  {
+    "revision": "d9154b4f1625d030b9fc3c9ff4e67ca0",
+    "url": "/spotify-project/static/media/DespacitoCover.d9154b4f.png"
+  },
+  {
+    "revision": "ff271b039b836f34607e2ae721ff799e",
+    "url": "/spotify-project/static/media/DespacitoRemixCoverEdit.ff271b03.jpg"
+  },
+  {
+    "revision": "25c5159c3fe0b670e2bcd4c08d9831de",
+    "url": "/spotify-project/static/media/Drake.25c5159c.png"
+  },
+  {
+    "revision": "1556dca55407f2b82665fe86edb4f66a",
+    "url": "/spotify-project/static/media/EdSheeran.1556dca5.png"
+  },
+  {
+    "revision": "cecddfdf0510b68d9f9bdaf7b0fee07c",
+    "url": "/spotify-project/static/media/EdSheeranPicEdit.cecddfdf.png"
+  },
+  {
+    "revision": "2d9e61f2289e7a42a833108d4ab6ada1",
+    "url": "/spotify-project/static/media/HumbleCover.2d9e61f2.jpg"
+  },
+  {
+    "revision": "6a3a0dd6c5013003149b677b3c292c86",
+    "url": "/spotify-project/static/media/IDontWannaLiveForeverCover.6a3a0dd6.png"
+  },
+  {
+    "revision": "22e25a313e7cba95af3726a2ebe7bf1f",
+    "url": "/spotify-project/static/media/ImTheOneCover.22e25a31.jpg"
+  },
+  {
+    "revision": "9bf583db2e80f755cb4b8d26f127ca9a",
+    "url": "/spotify-project/static/media/ImagineDragons.9bf583db.png"
+  },
+  {
+    "revision": "d45f40ead959226ae457272e98fe73dc",
+    "url": "/spotify-project/static/media/ItAintMeCover.d45f40ea.png"
+  },
+  {
+    "revision": "f8eaedb664ed524c9ea9cef0c5f06272",
+    "url": "/spotify-project/static/media/KendrickLamar.f8eaedb6.png"
+  },
+  {
+    "revision": "e5f92932c6c45f14355fbf670d7f5318",
+    "url": "/spotify-project/static/media/Khalid.e5f92932.png"
+  },
+  {
+    "revision": "d046d223fd270e11f95ace02fe80eb7e",
+    "url": "/spotify-project/static/media/LuisFonsi.d046d223.png"
+  },
+  {
+    "revision": "f8b1df2ce03423df676c0b4e05188f44",
+    "url": "/spotify-project/static/media/MaroonFive.f8b1df2c.png"
+  },
+  {
+    "revision": "03c6fff04b4334aaa9970a806d355432",
+    "url": "/spotify-project/static/media/MartinGarrix.03c6fff0.png"
+  },
+  {
+    "revision": "d3bf3828b9a8e39fbcf8e15a8f3a043a",
+    "url": "/spotify-project/static/media/PostMalone.d3bf3828.png"
+  },
+  {
+    "revision": "e3a20664d26c646dccc3e7c9e8212beb",
+    "url": "/spotify-project/static/media/ShapeOfYouCover.e3a20664.png"
+  },
+  {
+    "revision": "3578611f8dbb6fe658bdfa84223e9722",
+    "url": "/spotify-project/static/media/SomethingJustLikeThisCover.3578611f.png"
+  },
+  {
+    "revision": "5f0da06c211357575d2356e3193f5999",
+    "url": "/spotify-project/static/media/ThatsWhatILikeCover.5f0da06c.jpg"
+  },
+  {
+    "revision": "86c03861debc5dbd356381b5ad75d789",
+    "url": "/spotify-project/static/media/TheWeeknd.86c03861.png"
+  },
+  {
+    "revision": "f05eed9c14430807820013aec4dcb26d",
+    "url": "/spotify-project/static/media/UnforgettableCover.f05eed9c.jpg"
+  },
+  {
+    "revision": "dd8669dfe084e98d21ccc6e25eda52e0",
+    "url": "/spotify-project/static/media/Zayn.dd8669df.png"
+  }
+]);
